66867ddbb333b32d59e91507188edc15caf44b7c
Lucas Lima <lukkasoliveiralima@gmail.com>
2023-05-11 16:12:20 -0300

    add feature get servers by host

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/search-info-site
+module github.com/go-cli-app
 
 go 1.20
 
--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	app.Commands = []cli.Command{
 		{
 			Name:  "ip",
-			Usage: "Search addresses ips on web",
+			Usage: "Search addresses ips on web by host",
 			Flags: []cli.Flag{
 				cli.StringFlag{
 					Name:  "host",
@@ -34,6 +34,28 @@ func main() {
 
 				for _, ip := range ips {
 					fmt.Println(ip)
+				}
+			},
+		},
+		{
+			Name:  "server",
+			Usage: "Search servers names on web by host",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com",
+				},
+			},
+			Action: func(c *cli.Context) {
+				hostParam := c.String("host")
+
+				servers, err := net.LookupNS(hostParam)
+				if err != nil {
+					log.Fatal("Get servers error: ", err)
+				}
+
+				for _, server := range servers {
+					fmt.Println(server)
 				}
 			},
 		},
