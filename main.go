34a33a9ede94a6bbdf42421614b12783ff23eaec
Lucas Lima <lukkasoliveiralima@gmail.com>
2023-05-11 16:03:31 -0300

    add feature get ips by host

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net"
 	"os"
 
 	"github.com/urfave/cli"
@@ -10,6 +13,31 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Go cli app"
 	app.Usage = "Search public IP or server name by host using cli"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Search addresses ips on web",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com",
+				},
+			},
+			Action: func(c *cli.Context) {
+				hostParam := c.String("host")
+
+				ips, err := net.LookupIP(hostParam)
+				if err != nil {
+					log.Fatal("Get ips error: ", err)
+				}
+
+				for _, ip := range ips {
+					fmt.Println(ip)
+				}
+			},
+		},
+	}
 
 	app.Run(os.Args)
 }
